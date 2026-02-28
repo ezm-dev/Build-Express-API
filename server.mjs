6d6f7981d1ffd142f0e2d0bbb38d2e5e93e12710
ezm-dev <ezm.sabry@gmail.com>
2026-02-28 23:24:59 +1000

    Finalise filtering and  CORS

--- server.mjs
+++ server.mjs
@@ -1,13 +1,13 @@
 import express from "express"
 import { apiRoutes } from "./routes/apiRoutes.js"
-
+import cors from "cors"
 
 //app hold instance of express server to handel requests and responses
 const app = express()
 const PORT = 3000
 
 //middleware to enable CORS (Cross-Origin Resource Sharing) for the server, allowing it to handle requests from different origins.
-
+app.use(cors())
 
 //route handler for GET request to the URL "/api" with optional query parameters for filtering startups based on industry, country, continent, funding status, and MVP status. The filtered results are returned as a JSON response.
 app.use("/api",apiRoutes)
