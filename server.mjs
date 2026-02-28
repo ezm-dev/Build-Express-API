8c8bc061d7efe2058fbdecc877e436e30aba392e
ezm-dev <ezm.sabry@gmail.com>
2026-02-28 21:14:57 +1000

    Adding data

--- server.mjs
+++ server.mjs
@@ -1,20 +1,37 @@
 import express from "express";
-import { startups } from "./data";
+import { startups } from "./data/data.js"
 
 //app hold instance of express server to handel requests and responses
-const app = express();
-const PORT = 3000;
+const app = express()
+const PORT = 3000
 
-const person={
-    name:"Eman",
-    age: 25,
-    city: "Cairo"
-}
 
 
 //route handler for GET request to the root URL
-app.get("/",(req,res)=>{
-    res.json(person)
+app.get("/api",(req,res)=>{
+    let filteredStartups = startups; // Initialize with all startups
+     const {industry, country, continent, is_seeking_funding, has_mvp} = req.query
+    if(industry){
+        filteredStartups = filteredStartups.filter(startup => startup.industry.toLowerCase() === industry.toLowerCase())
+    }
+    if(country){
+        filteredStartups = filteredStartups.filter(startup => startup.country.toLowerCase() === country.toLowerCase())
+    }
+    if(continent){
+        filteredStartups = filteredStartups.filter(startup => startup.continent.toLowerCase() === continent.toLowerCase())
+    }
+    if(is_seeking_funding !== undefined){
+       // filteredStartups = filteredStartups.filter(startup => startup.is_seeking_funding === (is_seeking_funding === "true"))
+       filteredStartups = filteredStartups.filter(startup => startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase()))// The JSON.parse method converts the string "true" or "false" to a boolean value
+    }
+    if(has_mvp !== undefined){
+       // filteredStartups = filteredStartups.filter(startup => startup.has_mvp === (has_mvp === "true"))
+       filteredStartups = filteredStartups.filter(startup => startup.has_mvp === JSON.parse(has_mvp.toLowerCase()))
+   
+    }
+    res.json(filteredStartups)
+    //console.log(req.query)
+   // res.json(startups) //all data without filtering
     //res.send("Hello World");
 })
 
